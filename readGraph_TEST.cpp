51d1497692afc54d8039846f1346d1530b3d2ad3
llliting <hllliting@gmail.com>
2019-12-01 19:51:50 -0500

    update test readgraph

--- readGraph_TEST.cpp
+++ readGraph_TEST.cpp
@@ -20,6 +20,13 @@ TEST_CASE("TESTING readGraph Class ")
         input.open("graph.txt");
         int numV = readGraph(input, matrix, vLabel, eLabel);
         REQUIRE(numV == 4);
+        //for(int i = 0; i < numV; i ++){
+         //   delete matrix[i];
+         //   delete eLabel[i];
+        //}
+        delete [] vLabel;
+        delete [] eLabel;
+        delete [] matrix;
     }
 
     SECTION("TESTING readGraph2")
@@ -30,7 +37,35 @@ TEST_CASE("TESTING readGraph Class ")
         string** eLabel;
         ifstream input;
         input.open("graph.txt");
-        readGraph(input, adj, weights, lengths, vLabel, eLabel);
+        int numV = readGraph(input, adj, weights, lengths, vLabel, eLabel);
         REQUIRE(adj[0][0] == 3);
+       // for(int i = 0; i < numV; i ++){
+         //   delete adj[i];
+         //   delete eLabel[i];
+         //   delete weights[i];
+       // }
+        delete [] vLabel;
+        delete [] eLabel;
+        delete [] weights;
+        delete [] adj;
+    }
+
+    SECTION("TESTING readGraph3")
+    {
+        int** edgeList, numEdges;
+        double * weights;
+        string* vLabel;
+        string* eLabel;
+        ifstream input;
+        input.open("graph.txt");
+        int numV = readGraph(input, edgeList, weights, numEdges, vLabel, eLabel);
+        REQUIRE(edgeList[3][0] == 2);
+       // for(int i = 0; i < numV; i ++){
+       //     delete edgeList[i];
+       // }
+        delete [] vLabel;
+        delete [] eLabel;
+        delete [] weights;
+        delete [] edgeList;
     }
 }
